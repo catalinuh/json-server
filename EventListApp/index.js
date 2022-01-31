132dbbfe88ed1974aa2653e131d4de8e25a2cb45
Catalina <catalinamcquade@gmail.com>
2022-01-31 17:30:36 -0500

    finish Add Feature (almost)

--- EventListApp/index.js
+++ EventListApp/index.js
@@ -5,14 +5,23 @@ const wikiApi = (() => {
   const getEvents = () =>
     fetch([baseURL, path].join('/')).then((response) => response.json());
 
+  const deleteEvent = (id) => {
+    fetch([baseURL, path, id].join('/')),
+      {
+        method: 'DELETE',
+      };
+  };
+
   return {
     getEvents,
+    deleteEvent,
   };
 })();
 
 // View
 const view = (() => {
   const domStr = {
+    addNewBtn: '#add-new-btn',
     eventList: '#event-list__container',
     event: '.event',
     eventField: '.event-field',
@@ -20,6 +29,8 @@ const view = (() => {
     endField: '.end-field',
     editBtn: '.edit-btn',
     deleteBtn: '.delete-btn',
+    saveBtn: '.save-btn',
+    closeBtn: '.close-btn',
   };
 
   const render = (element, tmp) => {
@@ -37,7 +48,7 @@ const view = (() => {
             <input value=${elem.endDate} class="end-field" type="text" aria-label="End date" disabled />
             <div id="edit-delete">
                 <button class="edit-btn">Edit</button>
-                <button class="delete-btn">Delete</button>
+                <button class="delete-btn" id=${elem.id}>Delete</button>
             </div>
         </section>
           `;
@@ -56,11 +67,11 @@ const view = (() => {
 // Model
 const model = ((api, view) => {
   class Event {
-    constructor(eventName) {
+    constructor(eventName, startDate, endDate, id) {
       this.eventName = eventName;
       this.startDate = startDate;
       this.endDate = endDate;
-      this.id = 0;
+      this.id = id;
     }
   }
 
@@ -81,11 +92,15 @@ const model = ((api, view) => {
   }
 
   const getEvents = api.getEvents;
+  const addEvent = api.addEvent;
+  const deleteEvent = api.deleteEvent;
 
   return {
     Event,
     State,
     getEvents,
+    addEvent,
+    deleteEvent,
   };
 })(wikiApi, view);
 
@@ -100,8 +115,55 @@ const controller = ((model, view) => {
     });
   };
 
+  const addEvent = () => {
+    const newFields = document.createElement('section');
+    newFields.id = 4; // not dyanmic
+    newFields.innerHTML = `
+        <section class="event">
+            <input class="event-field" type="text" aria-label="Event name" />
+            <input class="start-field" type="text" aria-label="Start date" />
+            <input class="end-field" type="text" aria-label="End date" />
+            <div id="edit-delete">
+                <button class="save-btn">Save</button>
+                <button class="close-btn">Close</button>
+            </div>
+        </section>
+    `;
+
+    let element = document.getElementById('event-list__container');
+
+    const addNewBtn = document.querySelector(view.domStr.addNewBtn);
+    addNewBtn.addEventListener('click', () => {
+      element.appendChild(newFields);
+
+      const saveBtn = document.querySelector(view.domStr.saveBtn);
+      saveBtn.addEventListener('click', () => {
+        console.log('you clicked save!');
+      });
+
+      const closeBtn = document.querySelector(view.domStr.closeBtn);
+      closeBtn.addEventListener('click', () => {
+        const toBeRemoved = document.getElementById(4);
+        element.removeChild(toBeRemoved);
+      });
+    });
+  };
+
+  // incomplete
+  const deleteEvent = () => {
+    const elem = document.querySelector(view.domStr.deleteBtn);
+
+    elem.addEventListener('click', (e) => {
+      state.eventList = state.eventList.filter((event) => {
+        return +event.id !== +e.target.id;
+      });
+      model.deleteEvent(e.target.id);
+    });
+  };
+
   const bootstrap = () => {
     init();
+    addEvent();
   };
 
   return { bootstrap };
